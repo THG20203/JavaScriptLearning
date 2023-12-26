ca10b1417e1baeefdbb2ed74ba5fad4d3b014abd
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-26 23:22:04 +0000

    working with varaibles and values basic definition

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ console.log(40 + 8 + 23 - 10);
 
 /* persons used as examples */
 /* value = piece of data, fundamental unit of info in programming. i.e. Jonas is a value */
-("Jonas");
+console.log("Jonas");
+
+/* values can be stored into variables - to be reused over and over */
+/* let firstName = "Jonas"; is declaring a variable. creates real variable in computers memory. */
+let firstName = "Jonas";
