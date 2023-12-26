7b86fc3774f300c00c9bd21a7948a06c106ef034
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-26 19:30:18 +0000

    creation of a section txt file

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
 js = "boring";
 if (js === "amazing") alert("JavaScript is Fun!");
-
-/* JavaScript = high level object oriented, multi paradigm programming language */
-// high level - don't have to think about memory management whilst javascript runs our program
-// object oriented - mostly based on objects, for storing most kinds of data
-// multi paradigm - flexible, can use different programming styles like imperitive and declerative
-// programming language - tool instruct computer to do things
-
-/* Web Development? */
-// HTML = content of page - text, images, buttons
-// CSS = presentation of content - styling, laying out elements on webpage
-/* JavaScript  = programming language - allows developers dynamic and interactive effects to any webpage.
-Also use it manipulate the content or CSS, load data from remote server and build entire applications 
-in the browser - we then call web applications. */
