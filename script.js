7d403dd9806dd729f863e33b29c25791713b2033
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-26 19:29:18 +0000

    breaking down programming languages of the web

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ if (js === "amazing") alert("JavaScript is Fun!");
 // object oriented - mostly based on objects, for storing most kinds of data
 // multi paradigm - flexible, can use different programming styles like imperitive and declerative
 // programming language - tool instruct computer to do things
+
+/* Web Development? */
+// HTML = content of page - text, images, buttons
+// CSS = presentation of content - styling, laying out elements on webpage
+/* JavaScript  = programming language - allows developers dynamic and interactive effects to any webpage.
+Also use it manipulate the content or CSS, load data from remote server and build entire applications 
+in the browser - we then call web applications. */
