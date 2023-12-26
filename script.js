95856953f89073e57d4f158ade15f43f8ee1b0c8
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-26 19:24:31 +0000

    high level overview definition

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 js = "boring";
 if (js === "amazing") alert("JavaScript is Fun!");
+
+/* JavaScript = high level object oriented, multi paradigm programming language */
+// high level - don't have to think about memory management whilst javascript runs our program
+// object oriented - mostly based on objects, for storing most kinds of data
+// multi paradigm - flexible, can use different programming styles like imperitive and declerative
+// programming language - tool instruct computer to do things
