8d243b19feb21bc13918218aaaff2c57022cc448
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-26 23:05:33 +0000

    values initial

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let js = "amazing";
-if ((js = "amazing")) alert("JavaScript is fun!");
-
 console.log(40 + 8 + 23 - 10);
+
+/* persons used as examples */
+/* value = piece of data, fundamental unit of info in programming. i.e. Jonas is a value */
+("Jonas");
