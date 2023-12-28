26c68cf0283b696dffdb7e996b278e736d341d01
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 16:02:40 +0000

    initial boolean notes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+/* can store booleans in variables */
+let javascriptIsFun = true;
+/* remember, variable declaration needs to happen before the log, because javascript is executed
+from top to bottom */
+console.log(javascriptIsFun);
