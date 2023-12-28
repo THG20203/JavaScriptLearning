1e0c97dd1a2597f78e892466e0967393176e8e44
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 15:58:49 +0000

    fresh script.js for data types

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-let js = "amazing";
-/* whatever the operation could be summarised as below would be value, cause value is smallest 
-unit of information */
-console.log(40 + 8 + 23 - 10);
 
-/* persons used as examples */
-/* value = piece of data, fundamental unit of info in programming. i.e. Jonas is a value */
-console.log("Jonas");
-
-/* values can be stored into variables - to be reused over and over */
-/* let firstName = "Jonas"; is declaring a variable. creates real variable in computers memory. */
-let firstName = "Jonas";
-/* can now put in variable name */
-console.log(firstName);
