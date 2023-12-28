618a154d4dbf13b4436aa27caa087627f817e51e
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 22:09:07 +0000

    looking into more operators

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
+
+/* other operators -> times and divide, two operators in console.log cause we can have 
+more than one value in a console.log */
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// please note from above code -> 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
