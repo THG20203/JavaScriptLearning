362dd45365cb21464c86eca149fdea073700004f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 21:37:52 +0000

    finishing off const and let

--- script.js
+++
@@ -1,5 +0,0 @@
-/* use const - declare variables that are not supposed to change at any point in the future */
-/* value in a const variable cannot be changed */
-const birthYear = 1991;
-birthYear = 1990;
-/* this produces TypeError - Assignment to constant variable */
