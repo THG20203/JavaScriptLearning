b6e30bea9a784b88978da550106c33a308033ab6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 19:49:29 +0000

    data types null bug

---
+++ script.js
@@ -0,0 +1,5 @@
+/* error typeof operator - typeof null - similar to undefined. both the value and 
+the type of the value are null. */
+console.log(typeof null);
+/* JavaScript will say the type of null (above) is object. Error in JavaScript - never 
+corrected. */
