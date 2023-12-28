8dd12c706e426f821caf20be61bad114b1cf836c
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 19:49:49 +0000

    clean script.js again

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-/* error typeof operator - typeof null - similar to undefined. both the value and 
-the type of the value are null. */
-console.log(typeof null);
-/* JavaScript will say the type of null (above) is object. Error in JavaScript - never 
-corrected. */
+
