9be034ecbf576152fb4e61a014c96bb979fcbd3f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 21:42:56 +0000

    changing placement of code var explanation

--- script.js
+++
@@ -1,6 +0,0 @@
-/* var legacy reasons - important. Old way writing variables prior to ES6. */
-/* Works similar to let -> can mutate. */
-var job = "programmer";
-job = "teacher";
-
-/* let is blockscoped, var is function scoped */
