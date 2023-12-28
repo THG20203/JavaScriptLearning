030c0a8c4cb6219423f39e608429458552156626
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 16:10:24 +0000

    typeof

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let javascriptIsFun = true;
 /* remember, variable declaration needs to happen before the log, because javascript is executed
 from top to bottom */
 console.log(javascriptIsFun);
+
+/* typeof is an operator like + or - operator that we can use to the type of a value */
+/* console.log typeof true -> will show new value in console, which is a string which is the typeof 
+value */
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Tristan");
