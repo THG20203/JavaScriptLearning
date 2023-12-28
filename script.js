ea4a6f0ce96f6615131c8e76be76162d384101d8
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-28 22:05:06 +0000

    do not repeat using variables minus

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-/* Mathematical operator -> use minus operator to solve simple problem. */
-const ageJonas = 2037 - 1991;
-const ageSarah = 2037 - 2018;
+// Mathematical operator -> use minus operator to solve simple problem.
+/* lets have a now const variable called now, assign current year 2037 to the now variable so don't 
+have to repeat it */
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
