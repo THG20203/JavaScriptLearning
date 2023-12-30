90d70f21d3618768889c79a2c01980b4985b6839
THG20203 <tristanhgriffiths8@yahoo.com>
2023-12-30 13:44:07 +0000

    looking through comparison operators

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
-/* Comparison Operators -> produce boolean values. */
-console.log(ageJones > ageSarah);
+/* Comparison Operators used to produce boolean values. */
+/* The boolean below will produce true cause Jonas is older than Sarah 
+-> 2037 - 1991 vs 2037 - 2018 */
+/* below -> asking operator - is age of jonas greater than age of Sarah. 
+Operator will reply with yes (true) or no (false). */
+console.log(ageJonas > ageSarah);
+
+/* > greater */
+/* < less than */
+/* >=, greater equal */
+/* <= less than equal */
