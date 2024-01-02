087bc098110c6afb72d490a97d753bbbbeffab7f
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-02 21:31:53 +0000

    typo fixed, ages console.log

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ within parenthese are also executed first.
 Great example is to calculate the average of two values. Goign to add ageJonas and 
 ageSarah back in as variables. */
 /* ageJonas and ageSarah -> calculate the average age of these two people */
-const now = 27;
+const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
+
+/* start by logging their ages to the console. */
+console.log(ageJonas, ageSarah);
