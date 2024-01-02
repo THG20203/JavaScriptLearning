fb46535a52c94bada8ebc2bbcd4e3dc3c275f10c
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-02 21:40:05 +0000

    average explanation, setting out beginnings of use case for parentheses

--- script.js
+++ script.js
@@ -9,5 +9,14 @@ const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 
-/* start by logging their ages to the console. */
-console.log(ageJonas, ageSarah);
+/* calculate average age -> don't forget descriptive age. averageAge -> describes exactly whast this 
+variable will hold. To calculate average -> add two values together and divide by 2 (when have two 
+variables). */
+const averageAge = ageJonas + ageSarah / 2;
+/* notes -> what is going to happen? -> look at precedence table */
+/* we have the division (15) happening before the subtraction (14). So what'll happen is 
+age sarah will be divided by two, then ageJonas will be added */
+
+/* start by logging their ages to the console. then after third comma we're evaluating theory 
+above by adding averageAge to the console. */
+console.log(ageJonas, ageSarah, averageAge);
