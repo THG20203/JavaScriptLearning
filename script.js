f4a964111c9cdb3ef340215d8e1475c6d7f3626c
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-02 18:39:38 +0000

    posing console.log question

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
+
+/* Question asked at end of the last video was why do these two subtractions 
+'now - 1991'
+and 
+'now - 2018'
+are executed before the '>' comparison operator.
+*/
+console.log(now - 1991 > now - 2018);
+
+/* The question is why does the console.log code above work. */
