5a916df1973dea63f455311905ef3a6d008e9f7b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-02 21:53:42 +0000

    further average age explanation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const ageSarah = now - 2018;
 /* calculate average age -> don't forget descriptive age. averageAge -> describes exactly whast this 
 variable will hold. To calculate average -> add two values together and divide by 2 (when have two 
 variables). */
-const averageAge = ageJonas + ageSarah / 2;
+// const averageAge = ageJonas + ageSarah / 2;
 /* notes -> what is going to happen? -> look at precedence table */
 /* we have the division (15) happening before the subtraction (14). So what'll happen is 
 age sarah will be divided by two, then ageJonas will be added */
@@ -20,3 +20,12 @@ age sarah will be divided by two, then ageJonas will be added */
 /* start by logging their ages to the console. then after third comma we're evaluating theory 
 above by adding averageAge to the console. */
 console.log(ageJonas, ageSarah, averageAge);
+/* averageAge in the console above is 55.5 which is result of ageSarah / 2 + ageJonas because of order
+precedence with ageJonas being more important. 55.5 doesn't make any sense cause how can average
+be larger than two ages which are 46 for ageJonas and 29 for ageSarah
+is because:
+19 / 2 = 9.5 (ageSarah / 2) 
+then 9.5 + 46 (ageJonas) = 55.5 */
+
+/* so we should make sure ageJonas + ageSarah operation is completed first. */
+const averageAge = (ageJonas + ageSarah) / 2;
