22fdfed8b07a9354ac45bb6200da6f83ac909f44
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-02 21:22:46 +0000

    file preparation

--- script.js
+++ script.js
@@ -57,3 +57,5 @@ x = y = 25 - 10 - 5;
 /* why is the right to left rather than left to right? */
 /* if it was left to right then x would be equal to y, which would be undefined,
 because declared as an empty variable then y would be equal to 10. */
+
+/* note on highest precedence -> grouping */
