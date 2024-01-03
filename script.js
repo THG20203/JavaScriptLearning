2a6311c830befeafdcbf99433fdb1566057224bd
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-03 22:41:00 +0000

    multi line template literal strings

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* we can use backticks for any regular string shown in the console.log below */
 console.log(`Just a regular string..`);
+
+/* for multi line string previously needed \n\ in javascript for a new line */
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+
+/* with template literals -> we can drop it line by line just with an enter */
+console.log(`String
+multiple
+line`);
