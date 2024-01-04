8e7e031e5d53322facf0d582051ddf0b741f210b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-04 22:21:45 +0000

    starting template literals

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+/* write program -> checks whether a person is allowed to take a drivers license 
+or not. if yes will print to the console. if not will print how many years are left till the
+person can start taking the drivers license. */
 
+const age = 19;
+/* again varialbe name -> describes exactly what we want. greatOrEqual to 18 */
+const isOldEnough = age >= 18;
