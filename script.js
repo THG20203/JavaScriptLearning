bbc6e6ec6500a6cd1b1bed8c52b1c7eb0e3be1f5
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-04 22:49:21 +0000

    if else statement notes

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ person can start taking the drivers license. */
 const age = 19;
 /* again varialbe name -> describes exactly what we want. greatOrEqual to 18 */
 const isOldEnough = age >= 18;
+
+/* now have boolean value, can use to take decisions using if statement */
+/* if statement executed whenever the condition in parentheses is true */
+/* with condition mean anything that is a boolean value */
+// if() {
+/* if statment executed means code in the block -> as denoted by {} */
+// }
+
+/* using isOldEnough variable here in if statment as our CONDITION */
+/* We already know this will always be a BOOLEAN -> important:
+cause the value of a boolean needs to be true, so code in {} will execute */
