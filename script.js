9c013f03e17f619ab504cb0fde767336fcef9c27
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-04 23:48:28 +0000

    moving some content to fundamentals1.txt

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-/* write program -> checks whether a person is allowed to take a drivers license 
-or not. if yes will print to the console. if not will print how many years are left till the
-person can start taking the drivers license. */
-
 const age = 19;
-/* again variable name -> describes exactly what we want. greatOrEqual to 18 */
-// const isOldEnough = age >= 18;
-
-/* now have boolean value, can use to take decisions using if statement */
-/* if statement executed whenever the condition in parentheses is true */
-/* with condition mean anything that is a boolean value */
-// if() {
-/* if statment executed means code in the block -> as denoted by {} */
-// }
-
-/* using isOldEnough variable here in if statment as our CONDITION */
-/* We already know this will always be a BOOLEAN -> important:
-cause the value of a boolean needs to be true, so code in {} will execute.
-If flase not executed. */
-// if (isOldEnough) {
-//  console.log("Sarah can start driving license");
-// }
 
 /* in practice was always write condition in the if else statement parentheses */
 if (age >= 18) {
   console.log("Sarah can start driving lessons.");
+} else {
 }
