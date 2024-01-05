9d80c6ad6cffd36b25de024eda44d342b7e3c1bf
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-05 00:06:44 +0000

    adding else block

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const age = 19;
+const age = 15;
 
 /* in practice was always write condition in the if else statement parentheses */
 if (age >= 18) {
   console.log("Sarah can start driving lessons.");
-} else {
+} /* else will be executed whenever this condition is false */ else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years.`);
 }
