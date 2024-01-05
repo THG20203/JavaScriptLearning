48557d33b94ab899788ae58a5077be20c0b7a9b6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-05 00:10:53 +0000

    adding notes to script.js of if else theory

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ if (age >= 18) {
   const yearsLeft = 18 - age;
   console.log(`Sarah is too young. Wait another ${yearsLeft} years.`);
 }
+
+/* Note else blocks are optional. If theres is no else block, JavaScript 
+will quickly move onto the next line as if the condition is false. */
+
+/* if / else = control structure - this structure allows us to have more control
+over the way our code is executed.
+The whole code does not just execute in a linear way. JavaScript does not just execute
+code in a linear way. Instead we can now control blocks of code that should execute and 
+blocks that shouldn't */
