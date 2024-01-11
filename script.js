62a7dcd7c41d5542a6142aeba55e5ec1e0c9add2
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-11 23:53:42 +0000

    clearing script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 /* now lets create a variable conditionally and not just use console.log */
-
-let a = 1;
-y = a++;
-console.log(a);
-console.log(y);
-
-let b = 1;
-y = ++1;
-console.log(b);
-console.log(y);
-
