21d695a57e9fa3891f8449c7d7cf4ff685469434
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 15:11:56 +0000

    starting to look into if/ else statements again

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-/* now lets create a variable conditionally and not just use console.log */
+/* Another example of if/ else now lets create a variable conditionally and 
+not just use console.log */
+const birthYear = 1991;
+
+/* create another variable called century, will contain century in which this person
+was born. */
