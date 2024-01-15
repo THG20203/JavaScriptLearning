c0f03ab3c07c316b2956523073e199f1104a9727
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 23:20:50 +0000

    more type conversion code

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 come as strings */
 const inputYear = "1991";
 // will be 199118 if don't convert inputYear from string */
-console.log(inputYear + 18);
-console.log(Number(inputYear));
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
 /* Type coercion - javascript automatically converts types behind the scenes for us */
+
+/* below will give NaN - invalid number */
+console.log(Number("Jonas"));
+/* NaN typeof will be number - hence NaN maeaning invalid number rather than not a number */
+console.log(typeof NaN);
