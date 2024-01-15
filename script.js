e34c1a183bb6cdeabc43271555827431ff0216e6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 22:56:47 +0000

    starting look into type coercion

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 /* Type conversion - explicitly/ manually convert from one type to another */
-/* What if we have input field on webpage, user to input birthYear */
+/* What if we have input field on webpage, user to input birthYear - inputs usually 
+come as strings */
+const inputYear = "1991";
+// will be 199118 if don't convert inputYear from string */
+console.log(inputYear + 18);
+console.log(Number(inputYear));
 /* Type coercion - javascript automatically converts types behind the scenes for us */
