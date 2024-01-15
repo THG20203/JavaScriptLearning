6ff5c21c3e78213fa852ae752b5e03e7ca4e7dfb
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 15:38:43 +0000

    looking further into if else statements

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const birthYear = 1991;
 
 /* create another variable called century, will contain century in which this person
 was born. */
+
+/* to make this work, need to define century outside if/ else block */
+/* WHY? Any variable defined inside code block will not be accessible outside
+of the block. */
+if (birthYear <= 2000) {
+  let century = 20;
+} else {
+  let centruy = 21;
+}
+console.log(century);
+/* Proof of issue with defining variables inside of code block console.log produces
+Uncaught ReferenceError: century is not defined */
