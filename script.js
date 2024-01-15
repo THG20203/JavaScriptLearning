fba473b18c1b047d84301c18526193c684d70527
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 22:37:38 +0000

    type conversion and coercion definitions

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+/* Type conversion - explicitly/ manually convert from one type to another */
+/* What if we have input field on webpage, user to input birthYear */
+/* Type coercion - javascript automatically converts types behind the scenes for us */
