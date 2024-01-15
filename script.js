4c746fed3d34a6458fd4799d918abac980e3a16a
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-15 21:48:07 +0000

    sorting if else statement of challenge

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const heightJohn = 1.76;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+console.log(BMIMark, BMIJohn);
+
+/* write if else statement - could have written markHigherBMI, but more
+common to write condition directly in parentheses. */
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is higher than John's!");
+} else {
+  console.log("John's BMI is higher than Mark's");
+}
+
+/* see as a result of if statement in the console:
+John's BMI is higher than Mark's */
