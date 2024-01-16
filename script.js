97d67e5b8256460a4695c351446ee24983db3e46
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 23:44:25 +0000

    beginning boolean logic theory

---
+++ script.js
@@ -0,0 +1,15 @@
+/* when we learned about if else statements, comparison operators, 
+boolean values, we also need to learn about logic - in particular boolean logic. */
+
+/* boolean logic -> true and false values to solve complex logical problems */
+/* most basic boolean logic: the and, or & not operators */
+
+/* Example to utilise:
+A: Sarah has a driver's license
+B: Sarah has good vision
+
+A and B 
+"Sarah has a driver's license AND good vision.
+
+true when ALL are true - no matter how many variables.
+*/
