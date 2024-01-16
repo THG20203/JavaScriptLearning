9dc595c98c006a49794a9bafbdcfd9f8fe0a60d6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 21:33:15 +0000

    if else statment but with equality operator

---
+++ script.js
@@ -0,0 +1,8 @@
+/* So far only used comparison operators to take decisions with if/else statements
+but lets suppose if we want to check if two values are equal. */
+
+/* This is instead of one being greater or less than another. */
+
+const age = 18;
+/* person became an adult only if the age is 18 */
+if (age === 18) console.log("You just became an adult");
