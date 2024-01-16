4092350fe18e0c66805a00f1effa3d87af135c92
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 22:58:51 +0000

    looking into else if

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 /* pretty simple way of getting a value from a web page -> prompt function */
-/* we need to store this value somewhere - hence const favourite -> console.log */
-const favourite = prompt("What's your favourite number?");
+/* we need to store this value somewhere - hence const favourite -> console.log
+/* number converter */
+const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite); // logs the value you input in the prompt as a string
 // see that in typeof
 console.log(typeof favourite); // will equal string
 
-/* use lose equality operator initially, will do type coercion. */
-if (favourite == 23) {
-  // "23" == 23
+if (favourite === 23) {
   console.log("Cool! 23 is an amazing number");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
 }
