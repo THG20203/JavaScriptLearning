e240e4350279504037f7beb6a7b0cdd3aa8e6403
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 11:11:23 +0000

    starting to look into boolean console log

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 /* Falsy values -> aren't exactly false but will become false when try and convert them 
 into a boolean */
+/* JavaSceript = 5 falsy values -> 0, "", undefined, null, NaN*/
+
+/* 5 values above -> turn falsy when convert to a boolean. They will become falsy when converted
+to a boolean. */
+
+/* Everything else is a truthy values -> values that will be converted to true, i.e. any value that
+is not 0 will be converted to true when we attempt to convert them to a boolean. */
+
+console.log(Boolean(0)); // will equal false
