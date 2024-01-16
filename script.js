cfe9a6b9233e8c47a983d960215449f206f19e52
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 22:45:06 +0000

    explaining how type coercion is working with ==

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const favourite = prompt("What's your favourite number?");
 console.log(favourite); // logs the value you input in the prompt as a string
 // see that in typeof
 console.log(typeof favourite); // will equal string
+
+/* use lose equality operator initially, will do type coercion. */
+if (favourite == 23) {
+  // "23" == 23
+  console.log("Cool! 23 is an amazing number");
+}
