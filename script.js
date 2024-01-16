1ddbce051a8e44f7030d50c8f2a278cb1e5e7bcc
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 22:34:36 +0000

    setting up a prompt

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+/* pretty simple way of getting a value from a web page -> prompt function */
+/* we need to store this value somewhere - hence const favourite -> console.log */
+const favourite = prompt("What's your favourite number?");
+console.log(favourite);
