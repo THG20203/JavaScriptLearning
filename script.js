23b981e18cb144bfa3c97e37c880966656ebcc1a
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-16 22:38:39 +0000

    string type of in prompt

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /* pretty simple way of getting a value from a web page -> prompt function */
 /* we need to store this value somewhere - hence const favourite -> console.log */
 const favourite = prompt("What's your favourite number?");
-console.log(favourite);
+console.log(favourite); // logs the value you input in the prompt as a string
+// see that in typeof
+console.log(typeof favourite); // will equal string
