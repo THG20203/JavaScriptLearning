9a58a79aab5671c58c539c5493167b1c412a4bd8
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-17 22:34:22 +0000

    expression definition

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+/* Expression = piece of code that produces a value -> i.e. 3 + 4 */
+3 + 4;
+/* Number is an expression too i.e. 1991 -> will produce value in JS */
+1991;
+/* boolean code will also produce a value */
+true && false && !false;
 
+/* Statements = piece of code executed doesn't produce piece of value on 
+itself */
