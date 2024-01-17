159a91ea53134f0d52630c5bb8ee6c6794ce9569
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-17 23:02:44 +0000

    template literal continued

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ true && false && !false;
 
 /* Statements = piece of code executed doesn't produce piece of value on 
 itself */
+/* This if else statment below doesn't produce a value does it? All it 
+does is declares variable called str */
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+/* Template literal -> only insert expressions not statements. i.e. 2037 - 1991 
+as shown below */
+console.log(`I'm ${2037 - 1991} years old.`);
