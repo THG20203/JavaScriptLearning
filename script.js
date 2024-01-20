15d3ce966d675a49295c3e2aa0edcb680c2ff484
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-20 13:59:24 +0000

    ternary operator written

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+/* Already saw two ways of writing conditionals - if/else statment, switch 
+statement. Another one = conditional operator */
+/* Conditional statement lets us write conditional statements -> all in one
+line */
+const age = 26;
+/* Condition - is age at least 18. */
+/* Then ? mark -> if block. Code we want to be executed in
+case condition is true */
+/* Then : */
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to drink water");
