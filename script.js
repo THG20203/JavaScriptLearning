81b9c3e0a397381ca108538a7c6ffd460c7e4e2d
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-20 14:23:57 +0000

    code written with if else statement too

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 statement. Another one = conditional operator */
 /* Conditional statement lets us write conditional statements -> all in one
 line */
-const age = 13;
+const age = 10;
 /* Condition - is age at least 18. */
 /* Then ? mark -> if block. Code we want to be executed in
 case condition is true */
@@ -16,3 +16,19 @@ can conditionally declare variables */
 /* So more likely examplev -> returning values: */
 const drink = age >= 18 ? "wine" : "water";
 console.log(drink);
+
+/* the VALUE of drink its defined conditionally based on the condition above.
+All in one simple line. */
+/* Better than if statement -> cause if want to define variable inside 
+if / else block, we need to declare variable outside */
+
+/* For example in the code below, need to define drink2 variable outside 
+of if / else block.
+Any variable we define inside of a block is not available outside */
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
