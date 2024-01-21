b7be7529e9b20935494bbbb14ace7fb6add8ab8c
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-21 21:50:57 +0000

    going through parameters within functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+/* Lets explore functionality of functions */
+/* When we write functions - we pass data into a function, functions can 
+return data too. Can give us data back -> use for something else in the program. */
 
+/* in function below we also express something called parameters. Parameters 
+are like variables that are specific to this function. They will get defined once
+we call the function. */
+/* apples and oranges will be defined once the function is CALLED */
+/* apples and oranges represent the input data of this function */
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+}
+
+/* what we're doing above is using parameters - apples and oranges just as if 
+they were normal variables, just inside the fruitProcessor function. */
