62f3c36ea2070bd272e008baf01947f002c15e9b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-21 23:48:50 +0000

    trying further to understand fruitProcessor function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
 
+/* calling function again. 2 and 4 are specific arguments -> will get passed 
+into the function as parameters. */
+/* apples and oranges are the blank spaces. Then when we call function, we fill in
+these blank spaces. */
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
