61a7c5a013e140a01f232aaf5b5e617301a28ffb
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-21 23:21:59 +0000

    splitting up function notes

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
 
-fruitProcessor(5, 0);
