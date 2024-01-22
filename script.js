ddc2a0981436f50beab0a871ccdba02de59ea66f
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-22 21:34:46 +0000

    calling and capturing function

--- script.js
+++ script.js
@@ -6,10 +6,11 @@
 is like local variable only available inside of function. */
 function calcAge1(birthYear) /* then function body with curly braces */ {
   /* now code we want to create - calculate age. Current year - birthYear */
-  const age = 2024 - birthYear;
-  /* calculate age then return that value */
-  return age;
+  /* can just return age calculation. Dry principle - don't need age variable. */
+  return 2024 - birthYear;
 }
 
+/* calling value of parameter -> the ARGUMENT 1997 */
+/* const tristanAge -> capturing value into variable */
 const tristanAge = calcAge1(1997);
 console.log(tristanAge);
