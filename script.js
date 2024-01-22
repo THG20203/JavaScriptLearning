925349582c88a5642c49a56809b13ed702c726ba
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-22 22:43:48 +0000

    starting arrow functions

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-/* Whats the difference function declaration and expressions?
-Can call function declarations BEFORE they are defined in the code.
-*/
 
-// Function declaration
-/* calling function BEFORE defined in the code */
-const age1 = calcAge1(1991);
-
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-
-// Function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
-const age2 = calcAge2(1991);
-
-console.log(age1, age2);
