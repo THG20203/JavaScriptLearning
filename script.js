0a9ba1f9a3979ff9685c49dd3c7e52656ef8ee8d
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-23 09:51:42 +0000

    retirement function

--- script.js
+++ script.js
@@ -20,4 +20,23 @@ i.e. 2024 - birthYear */
 /* to use function? -> store it in variable -> const calcAge3 =  */
 const calcAge3 = (birthYear) => 2024 - birthYear;
 
-/* Arrow function = special form of function expression */
+/* arrow functions -> return happens implicitly */
+
+/* save return value into variable */
+const age3 = calcAge3(1991);
+console.log(age3);
+
+/* another more complex arrow function */
+/* How many years a person has left until retirement? */
+/* passing in birthYear. But to calculate based on birthYear alone, first
+need to calculate age, retirement age - currentAge. */
+const yearsUntilRetirement = (birthYear) => {
+  const currentAge = 2024 - birthYear;
+  /* give us how many years we have left */
+  const retirement = 65 - currentAge;
+  /* to return retirement, need to write return keyword explicitly. We can 
+  only ommit return if have one line function. */
+  return retirement;
+};
+/* could assign answer to variable, but instead console.log */
+console.log(yearsUntilRetirement(1997)); // 38 years
