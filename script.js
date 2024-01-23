dbd565dca6640a30da97a11213952be614889e5b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-23 23:42:04 +0000

    return statements return function proof

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Call one function inside of another function. */
 /* don't worry about the birth year variable also being used in this function 
 below. They are two different parameters or variables. */
 const calcAge = function (birthYear) {
-  return 2024 - birthYear;
+  return 2037 - birthYear;
 };
 
 /* we could even have a variable outside of the functions, could also be called
@@ -17,12 +17,16 @@ const yearsUntilRetirement = function (birthYear, firstName) {
   /* take decision based on retirmenet value */
   if (retirement > 0) {
     return retirement;
+    /* prove function exists if return statement is accurate and returned,
+    a console.log below it will be ignored */
+    console.log(`${firstName} retires in ${retirement} years.`);
   } else {
     /* In this case, if retirement is not greater than 0, the function will return -1.
     So, the purpose of returning -1 in the else block is to indicate that if retirement
     is not a positive value, it serves as a kind of "error" or "invalid" signal. */
     return -1;
+    console.log(`${firstName} has already retired.`);
   }
 };
 console.log(yearsUntilRetirement(1997, "Tristan"));
-console.log(yearsUntilRetirement(1970, "Mike"));
+console.log(yearsUntilRetirement(1950, "Mike"));
