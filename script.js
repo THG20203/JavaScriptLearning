8c04237d52de80d8eebb5981b9892c89fff496f6
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-23 23:59:53 +0000

    beginning recapping function types

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+/* Functions three different tyoes */
+/* declaration - function that can be used before declared. */
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+/* expression: */
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
 
+/* arrow: */
+const calcAge = (birthYear) => 2037 - birthYear;
