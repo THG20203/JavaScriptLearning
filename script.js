39ac53b802b4c9c9e0c51a5d8140eb0e78d5f6a4
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-23 23:05:35 +0000

    starting to go over functions again

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+/* export functionality into another function -> like calcAge function.
+Call one function inside of another function. */
+/* So lets write another function expression */
+/* don't worry about the birth year variable also being used in this function 
+below. They are two different parameters or variables. */
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
 
+/* we could even have a variable outside of the functions, could also be called
+  birthYear */
+
+/* re writing yearsTillRetirement function as regular function expression. */
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+};
