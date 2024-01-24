54304d608e30a6d3304461e529c9cd2148cf9ccb
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-24 00:32:01 +0000

    writing out calcAge for further notes

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const calcAge = function (birthYear) {
 
 /* arrow - great for quick one line functions. No arrow (more later). */
 const calcAge = (birthYear) => 2037 - birthYear;
+
+/* three different ways of writing functions, but all work in similar way:
+input data, transform data, output data */
+
+/* structure of common function */
+function calcAge(birthYear, firstName) {
+  const age = 2037 - birthYear;
+  console.log(`${firstName} is ${age} years old`);
+  return age;
+}
+const age = calcAge(1991, "Jonas");
