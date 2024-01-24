c573cf3e649d521f97ac03d7acd4ad98a0f4a868
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-24 22:37:53 +0000

    looking into common function

--- script.js
+++ script.js
@@ -15,9 +15,23 @@ const calcAge = (birthYear) => 2037 - birthYear;
 input data, transform data, output data */
 
 /* structure of common function */
+/* 1. usually function needs a function name => i.e calcAge */
+/* 2. Then function has parameters -> recieve input values. Like local 
+variables of a function. */
+/* 3. Function body: block of code that we want to reuse. Processes the
+function's input data. */
+/* 4. return statement - block of code that we want to re use. Processes 
+the function's input data. 
+Also -> return statement to output a value from the function and 
+terminate execution. */
 function calcAge(birthYear, firstName) {
   const age = 2037 - birthYear;
+  /* console.log is just another function call inside calcAge function */
   console.log(`${firstName} is ${age} years old`);
   return age;
 }
+/* 5. Calling, running or invoking the function using () */
+/* 6. Arguments inside () - actual values of function parameters, to input 
+data */
+/* 7. Variable to save returned value (function output) */
 const age = calcAge(1991, "Jonas");
