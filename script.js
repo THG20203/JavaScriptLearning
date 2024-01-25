003bc6db9062ac2b64b475775e2a962e4eeebaf1
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-25 00:29:25 +0000

    starting array notes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/* Data structure -> arrays:
+Store friends names in variables -> use later in program. */
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
 
+/* Imagine if wanted 10 friends, would need 10 variables */
+
+/* Data structures like arrays. Arrays are big containers, throw 
+varialbes and later reference them */
