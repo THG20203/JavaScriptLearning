1ffc0aecc77e84e2e67003337747ba3f1d77ea27
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-30 20:54:08 +0000

    starting array notes again

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-/* Data structure -> arrays:
-Store friends names in variables -> so could use later in program. */
-/* Knowledge so far, this is how we'd do it */
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
 
-/* Imagine if wanted to represent 10 friends, would need 10 variables. 
-Instead wouldn't it be good to bundle all of values together into a bigger 
-container. */
-
-/* Thats why we have data structures like arrays in javascript */
-
-/* Data structures like arrays. Arrays are big containers, throw 
-variables and later reference them */
