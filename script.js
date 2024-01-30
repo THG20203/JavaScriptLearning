e8c2beb9026323d0471c53ba522b6d6099bbde91
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-30 21:11:14 +0000

    examined arrays in console

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* create variable called friends, with []. In here we can put 
 different values seperated by commas. */
-/* Format should be "String", so string then comma */
+/* Format should be "String", so string then comma and then another value. */
 const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
