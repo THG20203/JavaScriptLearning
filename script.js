0fb992187b618ba3d1c2a2d2a05f43de02ba435e
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-30 21:08:55 +0000

    an array written

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+/* create variable called friends, with []. In here we can put 
+different values seperated by commas. */
+/* Format should be "String", so string then comma */
+const friends = ["Michael", "Steven", "Peter"];
