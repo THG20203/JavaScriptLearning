3cd0707836243a13ef089bd397187f13f494c392
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-30 23:50:05 +0000

    arrays can be muttable because not primitive types

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-/* Square brackets not only for retreiving elements from the array, 
-but we can also change it to add elements to the array. */
 
-const friends = ["Michael", "Steven", "Peter"];
-/* can mutuate arrays */
-friends[2] = "Jay";
-console.log(friends); // (3) ["Michael", "Steven", "Jay"]
+
