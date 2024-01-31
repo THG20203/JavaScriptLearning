9bf9ff42a4cdfd9f2541926b9f802772f67b09ab
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-31 22:33:08 +0000

    function calls in array

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ years array. */
 /* we cannot do operations with arrays: */
 console.log(years + 10); // 1990, 1967, 2002, 2010, 201810
 console.log(years - 10); // NaN
+
+/* but we can get single values from the array */
+const age1 = calcAge(years[0]); // 1990 (this works)
+const age2 = calcAge(years[1]); // 1967 (this works)
+const age3 = calcAge(years[years.length - 1]); // get last value with .length - 1 trick
+console.log(age1, age2, age3);
+
+/* Take above further, started with array, good to end up with new array const ages */
+/* we can put function calls into an array cause they will produce a value */
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
