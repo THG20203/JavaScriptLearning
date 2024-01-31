32d570e3c3e6c17945b0a24da337a261b04332b9
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-31 00:10:21 +0000

    variables for names in arrays

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+/* JavaScript -> array can hold values with different types all at the
+same time. */
+/* In each position, JavaScript simply expects an expression. So array below will
+work. */
+const jonas = ["Jonas", "Course", 2037 - 1991];
 
-
+/* 2037 - 1991 will produce a value - so thats the value that will be stored at position
+number 2 in the new array. */
+/* This also works for variables, could also do */
+const firstName = "Tristan";
+const tristan = [firstName, "Griffiths", 2024 - 1997];
