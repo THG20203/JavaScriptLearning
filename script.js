6204c60aff507cc1966e252bab21a5caf45712e8
THG20203 <tristanhgriffiths8@yahoo.com>
2024-01-31 23:05:10 +0000

    push method arrays

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Array methods -> array operations */
+const friends = ["Michael", "Tristan", "Sam"];
+// push method -> add elements onto end of array
+friends.push("Jay");
+/* push is a function - see that by parenthese. Its a function we call, function 
+attached to the friends array itself. hence '.' */
+console.log(friends); // (4) ["Michael", "Tristan", "Sam", "Jay"]
