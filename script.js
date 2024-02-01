6aad677fff69513599f3212521fbc65030901d17
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-01 22:32:19 +0000

    taking away methods arrays

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* Array methods -> array operations */
 const friends = ["Michael", "Tristan", "Sam"];
+
+/* ADD ELEMENTS */
 // push method -> add elements onto end of array
 friends.push("Jay");
 /* push is a function - see that by parenthese. Its a function we call, function 
@@ -13,3 +15,22 @@ in the example friends.push("Jay") */
 So if want to capture that value -> can create new variable.  */
 const newLength = friends.push("Jay");
 console.log(newLength); // get the value 5 in the console
+
+friends.unshift("John");
+/*  The unshift method is used to add one or more elements to the beginning
+of an array. */
+console.log(friends); // (5) ["John", "Michael", "Steven", "Peter", "Jay"]
+/* unshift also returns value -> length of array */
+
+/* REMOVE ELEMENTS */
+/* pop is opposite to push -> so remove last element of the array */
+/* Don't need to pass an argument, cause no information needed to take off last 
+element of an array. */
+friends.pop();
+
+/* pop method also returns something but not length of new array,
+but instead it returns the removed element. */
+const popped = friends.pop();
+console.log(popped); // Last element (Jay) saved into popped
+
+friends.shift(); // Takes element off the start rather than end like pop() does
