fd78c41ed8fef854b3b00d8124c86042ab31a3ef
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-01 23:07:50 +0000

    array method function

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ console.log(friends.indexOf("Steven")); // 1 - cause index based.
 in the array, false if not. */
 console.log(friends.includes("Steven")); // True
 console.log(friends.includes("Joseph")); // False
+/* strict equality for includes check - so if had 23 number but checked for
+"23" string wouldn't work. It does strict equality. It DOES NOT do type coercion. */
+
+/* could do */
+if (friends.includes("Steven")) {
+  console.log("Includes Steven.");
+}
