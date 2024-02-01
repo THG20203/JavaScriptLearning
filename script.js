ef8341ef58d3a0c8bad86ac8a0bf1804261cca6b
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-01 23:00:36 +0000

    includes method

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+/* indexOf */
 const friends = ["Michael", "Steven", "Peter"];
 console.log(friends.indexOf("Steven")); // 1 - cause index based.
+
+/* includes -> instead of returning index of the element, true if
+in the array, false if not. */
+console.log(friends.includes("Steven")); // True
+console.log(friends.includes("Joseph")); // False
