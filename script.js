3e4175e45fd3b3b109d0062a0cdbfcbbcf8f22b5
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-06 21:42:13 +0000

    writing out array for objects explanation and intro

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+/* data structure -> combines multiple values, which all belong to tristan 
+entity tristanArray */
+const tristanArray = [
+  "Tristan",
+  "Griffiths",
+  2037 - 1997,
+  "Developer",
+  ["Sam", "John", "Peter"],
+];
