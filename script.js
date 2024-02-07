11c71440a542fda286b4f30ff93d24afee7abdde
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-07 20:27:56 +0000

    dot notation

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-/* Get properties from object, using property name itself */
+/* Get properties from object, using property name itself -> explain it 
+by writing out jonas object. */
+const jonas = {
+  firstName: "jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+/* DOT NOTATION */
+/* dot . is operator go to the property with name specified */
+console.log(jonas.lastName);
