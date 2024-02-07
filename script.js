f854a1786128f956a1a2416274ed385d07dd6c30
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-07 20:37:24 +0000

    lookign at bracket notation

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ console.log(jonas);
 /* DOT NOTATION */
 /* dot . is operator go to the property with name specified */
 console.log(jonas.lastName);
+/* BRACKET NOTATION */
+/* bracket notation in brackets can put any expression we like, so don't have
+to explicitly write string, can compute it from operation. Remember operation 
+is basically an expression. */
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+/* inside brackets -> could've constructed name using template literal, */
+console.log(jonas["first" + nameKey]);
