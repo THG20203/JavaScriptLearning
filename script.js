dd70a0df0bf7d8e8a32cf7889a74211a3835fd97
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 12:26:10 +0000

    beginning looking at for loops

---
+++ script.js
@@ -0,0 +1,8 @@
+/* loops automate repetitive tasks */
+/* for loops have 3 parts. 
+1). intial value of a counter. 
+(Use let value, cause later counter will be updated)
+2). logical condition evaluated before each iteration of the loop. 
+(So before each time the code is executed)
+3). */
+for(let rep = 1; ) {}
