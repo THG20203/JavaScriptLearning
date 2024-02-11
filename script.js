78d7a8b7ca8045e6010487f2100cd7d169694602
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 23:41:04 +0000

    comparing conditions for for loops and while loops

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+/* The while loop -> difference between for loop and while loop */
 
+/* for loop comparison */
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+/* while loop ->for the while loop, we can only specify a condition */
+/* so in this case whilst repetitions are less or equal to 10 */
+while (rep <= 10) {}
