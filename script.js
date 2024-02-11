e3cdbd72ca5730a4201f8dbead93b6b9f7237a2f
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 22:49:17 +0000

    how it looks in console.log

--- script.js
+++ script.js
@@ -3,6 +3,27 @@
 for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`----STARTING EXERCISE ${exercise}`);
   for (let rep = 1; rep < 6; rep++) {
-    console.log(`lifting weights repetition ${rep}`);
+    console.log(`lifting weight repetition ${rep}`);
   }
 }
+
+/* How will this look on the console.log? 
+------- Starting exercise 1
+Lifting weight repetition 1
+Lifting weight repetition 2
+Lifting weight repetition 3
+Lifting weight repetition 4
+Lifting weight repetition 5
+------- Starting exercise 2
+Lifting weight repetition 1
+Lifting weight repetition 2
+Lifting weight repetition 3
+Lifting weight repetition 4
+Lifting weight repetition 5
+------- Starting exercise 3
+Lifting weight repetition 1
+Lifting weight repetition 2
+Lifting weight repetition 3
+Lifting weight repetition 4
+Lifting weight repetition 5
+*/
