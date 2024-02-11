d31a1ec5165c035ffa8be33df854c61128013f52
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 17:34:40 +0000

    another array for loop example

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* another example array of birthYears */
+const years = [1991, 2007, 1969, 2020];
 
+/* now want to calculate ages for these birthYears, store them in an array */
 
+/* create an empty array to hold the ages - we're going to do ages.push() to
+push into the empty array. */
+const ages = [];
 
+for (let i = 0; i < years.length; i++) {
+  /* years[i] is years at current loop position */
+  ages.push(2037 - years[i]);
+}
