7ad8f3c1067449f40dff63f6fd0c3af4f0c055c9
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 22:35:36 +0000

    for loop in for loop written

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 /* loops in loops */
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`----STARTING EXERCISE ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`lifting weights repetition ${rep}`);
+  }
+}
