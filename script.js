a90b11b6ff17aa448a301eedfc98173da4454404
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 14:44:00 +0000

    sorting through what I want to achieve from the counter

---
+++ script.js
@@ -0,0 +1,23 @@
+/* more features of for loop -> loop through array */
+const jonasArray = [
+  "Jonas",
+  "Schmedmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+/* i is traditional counter name -> starting it at 0. This is because
+arrays 0 based. */
+for (let i = 0; ; i++) {
+  console.log(jonas[i]);
+}
+
+/* all we want to log to the console is each element of the array. So 
+what kind of code would we like to achieve:
+console.log(jonas[0])
+console.log(jonas[1])
+console.log(jonas[2])
+console.log(jonas[3])
+console.log(jonas[4])
+*/
