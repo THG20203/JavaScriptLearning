8bf20aaa5e1eb50e2bd938f5bdc484df16909332
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-11 22:57:21 +0000

    loop in loop

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* loops in loops */
-
 for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`----STARTING EXERCISE ${exercise}`);
   for (let rep = 1; rep < 6; rep++) {
@@ -27,3 +26,8 @@ Lifting weight repetition 3
 Lifting weight repetition 4
 Lifting weight repetition 5
 */
+
+/* start exercise 1 then within iteration of exercise 1 and new loop is created
+that then creates lifting weights repetition from 1 to 5. then iteration of exercise 
+2, then create lifting weights reptition 1 to 5 again etc. 
+Total of 15 repetitions. */
