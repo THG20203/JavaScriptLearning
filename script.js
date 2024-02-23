9c8c8e1f792b9b1f040e8f49ffab46f2bdc62247
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-23 23:05:53 +0000

    moving on to click events

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 "use strict";
 
-/* Besides getting text content, can set content to something else */
-/* below we will do it by writing = then whatever we want the new content to be */
-document.querySelector(".message").textContent = "Correct number";
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
+/* Lets make application do something when click on check button */
