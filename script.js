c6f5959fb5dd4db1d52623509b4f40419a227834
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-23 22:44:51 +0000

    beginning selecting and manipulating elements

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 "use strict";
+
+/* Besides getting text content, can set content to something else */
+/* below we will do it by writing = then whatever we want the new content to be */
+document.querySelector(".message").textContent = "Correct number";
