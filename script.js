57494d0f66927243b37d3a37e33b025f5e3015f0
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 23:04:31 +0000

    starting javascript in DOM

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+/* select elements we need and store into variables */
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+  });
+}
