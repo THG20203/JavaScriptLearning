cc49f71cbe5d0eb6d1a3d5e2b7a9968d8228d880
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 16:21:03 +0000

    sorting comments out

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 "use strict";
 
 /* SECRET NUMBER VARIABLE */
-/* defining secret number to guess against outside of check button, 
-cause only want to define it once when we start the application */
-/* Math.random = 0 - 1 */
-/* Math trunc -> get rid of any fractional digits */
-/* + 1 to take it from 0 - 19 to 1 - 20 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
 /* SCORE VARIABLE */
-/* Better way than reading score and changing it is creating a varaible, then updating
-that variable (decreasing it) then displaying it in score label. Good to have variable in 
-code and not just in the DOM. */
-/* let score 20 start at 20, make it a let so can decrease */
 let score = 20;
 
 /* GUESS BUTTON CLICK LOGIC */
