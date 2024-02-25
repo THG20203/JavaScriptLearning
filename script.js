c2c1a0e29ce6320f306195b7c3e6000e34de5620
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 16:00:59 +0000

    comments sorted

--- script.js
+++ script.js
@@ -20,10 +20,15 @@ let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number";
+
+    // When guess is too low
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high";
@@ -33,6 +38,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost the game";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When guess is too high
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too low";
