ec5e349c7e5bd789b8faf6a5211983c39504bf2e
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 14:43:12 +0000

    guess button logic complete

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ cause only want to define it once when we start the application */
 /* Math.random = 0 - 1 */
 /* Math trunc -> get rid of any fractional digits */
 /* + 1 to take it from 0 - 19 to 1 - 20 */
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low";
   }
 });
-
-/* Comparing number to the users guess */
