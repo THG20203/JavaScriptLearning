47d8351bb46845458dbd079d69173b08d041eab5
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 14:36:33 +0000

    starting game logic

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/* defining secret number to guess against outside of check button, 
+cause only want to define it once when we start the application */
+/* Math.random = 0 - 1 */
+/* Math trunc -> get rid of any fractional digits */
+/* + 1 to take it from 0 - 19 to 1 - 20 */
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -8,3 +16,5 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "No number";
   }
 });
+
+/* Comparing number to the users guess */
