56ad4d436f00b84dedaf31bf6a04f54e63d5fcdf
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-25 14:49:31 +0000

    score variable theory

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* SECRET NUMBER VARIABLE */
 /* defining secret number to guess against outside of check button, 
 cause only want to define it once when we start the application */
 /* Math.random = 0 - 1 */
@@ -7,6 +8,11 @@ cause only want to define it once when we start the application */
 /* + 1 to take it from 0 - 19 to 1 - 20 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
+
+/* SCORE VARIABLE */
+/* Better way than reading score and changing it is creating a varaible, then updating
+that variable (decreasing it) then displaying it in score label. Good to have variable in 
+code and not just in the DOM. */
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
