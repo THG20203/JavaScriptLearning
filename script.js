0b852ca7ef99b52946a9837e2a8f323f832aaccd
THG20203 <tristanhgriffiths8@yahoo.com>
2024-02-26 17:47:58 +0000

    modal work so far

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
+/* remember that the for loop structure is declared let with a variable z*/
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
-}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+/* no parentheses because don't want to call function initially until 
+after click s*/
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
